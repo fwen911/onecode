9402c463b55b9237fe574cb3390257bacc3607e8
fwen911 <911804361@qq.com>
2025-10-31 10:45:43 +0800

    Add files via upload

---
+++ 第一周helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+// HelloWorld 类（类名需与文件名一致）
+public class HelloWorld {
+    // 主方法：Java 程序的入口（固定格式，必须有）
+    public static void main(String[] args) {
+        // 打印 Hello World 到控制台（分号结尾，字符串用双引号）
+        System.out.println("Hello World!");
+    }
+}
